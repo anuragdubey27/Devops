e1374abaf538e68e2cdef58ef0c335b57d94ee7d
anuragdubey27 <145107208+anuragdubey27@users.noreply.github.com>
2023-09-15 13:48:00 +0530

    Add files via upload

---
+++ src/main/java/com/nagarro/Anurag/AnuragApplication.java
@@ -0,0 +1,20 @@
+package com.nagarro.Anurag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AnuragApplication {
+
+	@GetMapping("/test")
+	public String test() {
+		return "Hi, I am Anurag Dubey.!!!!!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(AnuragApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagarro/Anurag/AnuragApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.Anurag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnuragApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
